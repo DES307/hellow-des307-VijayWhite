fc125b81f3b79df0cc3497a6b5c6c5d8baca884a
VijayWhite <vsw001@student.usc.edu.au>
2020-03-12 13:50:48 +1000

    week 3

---
+++ js/week-3.js
@@ -0,0 +1,60 @@
+//object, method
+console.log('hello');
+
+//variable
+//var
+let number = 10;
+//const
+
+console.log(number);
+
+//primitive data type, string, number, boolea, null, underfined 
+
+//const firstname = "joe";
+//const lastname = "smith";
+//const age = "20";
+//const marriage = true; 
+
+//console.log();
+//console.log();
+
+/*const person = {
+    firstname: "Joe",
+    lastname: "Smith",
+    age: 20,
+    marriage: true,
+    address: {
+        unit: 6,
+        street: "Melbourne",
+    }
+};*/
+
+//console.log(person.firstname);
+
+/*const person = {
+    firstname: "Vijay",
+    lastname: "White",
+    age: 19,
+    gender: "male",
+    hobbies: ["gaming", "movies", ""],
+    address: {
+        street: "10 Cutler St",
+        suburb: "Central Lakes",
+        city: "Caboolture",
+        state: "Queensland",
+        postcode: 4510,
+    }
+}*/
+
+let score = 20;
+if (score === 100) {
+    console.log("cool");
+} else if (score >15 && score<20){
+    console.log("Stress is getting to you");
+} else if (score >21 && score<30){
+    console.log("Life is stress");
+} else {
+    console.log("")
+}
+
+
